4db86025f77050e2fd9189997724819a697f40e4
fabionobre <eu@fabionobre.com.br>
2016-08-23 09:00:10 -0300

    Initial Commit, almost done

---
+++ js/controllers/calculator.js
@@ -0,0 +1,85 @@
+var app = angular.module('calculator-app', ["calculator-app.controller"]);
+
+var controllers = angular.module('calculator-app.controller', [])
+
+.controller('calculator', function($scope, $window) {
+
+	$scope.total = 0;
+	$scope.subTotal = "";
+	$scope.nextAction = "";
+
+	$scope.addToSubTotal = function(i) {
+
+		if ($scope.subTotal.length >= 12) {
+			return;
+		}
+		
+		if ($scope.nextAction == "") {
+			$scope.total = 0;
+		}
+	
+		$scope.subTotal = $scope.subTotal + "" + i;
+	};
+
+	$scope.addDot = function() {
+
+		if (!$scope.subTotal.includes(".")) {
+			$scope.subTotal = $scope.subTotal + ".";
+		}
+	};
+
+	$scope.operetionClicked = function(operation) {
+
+		if ($scope.nextAction == "") {
+			$scope.nextAction = "+";
+		}
+
+		this.result();	
+		$scope.nextAction = operation;
+	}
+
+	$scope.clear = function() {
+		$scope.total = 0;	
+
+		// Clear temporary values
+		clearTemporarayValues();
+	}
+
+	$scope.result = function() {
+
+		if ($scope.subTotal == "") {
+			return;
+		}
+
+		switch($scope.nextAction) {
+		case "+":
+			$scope.total = $scope.total + parseFloat($scope.subTotal);
+			break;
+		case "-":
+			$scope.total = $scope.total - parseFloat($scope.subTotal);
+			break;
+		case "*":
+			$scope.total = $scope.total * parseFloat($scope.subTotal);
+			break;
+		case "/":
+			$scope.total = $scope.total / parseFloat($scope.subTotal);
+			break;
+		default:
+			return;
+		}		
+
+		if ($scope.total > 999999999999) {
+			$scope.total = 999999999999;
+		}
+
+		$scope.total = Math.round($scope.total * 100) / 100
+
+		// Clear temporary values
+		clearTemporarayValues();
+	}	
+
+	clearTemporarayValues = function() {
+		$scope.subTotal = "";
+		$scope.nextAction = "";
+	}
+})
